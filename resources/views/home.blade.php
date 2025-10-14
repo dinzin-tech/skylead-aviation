d17eab714e434e5e484422dba28e34e95b175752
vikkie <vivekdeshmukh1718@gmail.com>
2025-10-14 21:35:10 +0530

    changes made for map graphics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,10 @@
+
 @extends('layouts.app')
 
-@section('title', 'Home - Skylead Aviation')
+@section('title',  'Home - Skylead Aviation')
+@push('head')
+    <link rel="stylesheet" href="{{ asset('css/leaflet.css') }}" />
+@endpush
 
 @section('content')
     <!--================ Start Home Banner Area =================-->
@@ -43,7 +47,11 @@
     <!--================ End FAQ Area =================-->
 
 @endsection
+
+
 @push('scripts')
+    <script src="{{ asset('js/leaflet.js') }}"></script>
+
     <script>
         // FAQ Filtering and Interaction
         document.addEventListener('DOMContentLoaded', function() {
@@ -98,3 +106,62 @@
             // }
         });
     </script>
+
+    <script>
+    // Initialize Map
+    var map = L.map('map', {
+      minZoom: 2,
+      maxZoom: 6,
+      worldCopyJump: true
+    }).setView([50, 0], 2);
+
+    // Add Tile Layer
+    map.createPane('labels');
+    map.getPane('labels').style.zIndex = 650;
+    map.getPane('labels').style.pointerEvents = 'none';
+
+    var baseLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png').addTo(map);
+    var labelsLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
+      pane: 'labels'
+    }).addTo(map);
+
+    // Highlighted Countries
+    const highlightedCountries = ["India", "United States of America", "Australia", "South Africa", "Canada", "New Zealand"];
+
+    function style(feature) {
+      const name = feature.properties.name;
+      const isHighlighted = highlightedCountries.includes(name);
+      return {
+        weight: 1,
+        opacity: 1,
+        color: isHighlighted ? '#002533' : 'white',
+        dashArray: '3',
+        fillOpacity: 0.8,
+        fillColor: isHighlighted ? 'orange' : '#d9d9d9'
+      };
+    }
+
+    // Load GeoJSON Data
+    fetch('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json')
+      .then(response => response.json())
+      .then(data => {
+        L.geoJson(data, { style: style }).addTo(map);
+      });
+
+    // Add Pins
+    const pinnedCountries = [
+      { name: "India", lat: 20.5937, lng: 78.9629 },
+      { name: "USA", lat: 37.0902, lng: -95.7129 },
+      { name: "Australia", lat: -25.2744, lng: 133.7751 },
+      { name: "South Africa", lat: -30.5595, lng: 22.9375 },
+      { name: "Canada", lat: 56.1304, lng: -106.3468 },
+      { name: "New Zealand", lat: -40.9006, lng: 174.886 }
+    ];
+
+    pinnedCountries.forEach(country => {
+      L.marker([country.lat, country.lng])
+        .bindPopup(`<b>${country.name}</b>`)
+        .addTo(map);
+    });
+  </script>
+@endpush
